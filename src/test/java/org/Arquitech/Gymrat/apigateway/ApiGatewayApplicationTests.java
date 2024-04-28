64f870bbb52d76b189b6a9198b99e30486872ec8
RamiroVT <u202123714@upc.edu.pe>
2024-04-28 00:17:45 -0500

    fix: pom and yml files

---
+++ src/test/java/org/Arquitech/Gymrat/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Arquitech.Gymrat.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
